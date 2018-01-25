45300aa45c80d950aa8204e53a4b9780dff35d1a
zetx16 <zetx16@gmail.com>
2018-01-25 21:04:39 +0300

    Existing tags editor form implemented

--- planes.js
+++ planes.js
@@ -1,4 +1,4 @@
-var map = L.map('map', { fadeAnimation: false });
+﻿var map = L.map('map', { fadeAnimation: false });
 var hash = new L.Hash(map);
 
 if (document.location.href.indexOf('#') == -1)
@@ -95,6 +95,58 @@ new L.Control.Info({ position: 'topright' }).addTo(map);
 
 // ---------------------------------------------
 
+var osmstatic = `<?xml version="1.0" encoding="UTF-8"?>
+<osm version="0.6" generator="Overpass API 0.7.54.12 054bb0bb">
+<note>The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.</note>
+<meta osm_base="2018-01-23T17:47:02Z"/>
+<node id="4301705340" changeset="69643" timestamp="2016-01-08T14:40:04Z" version="1" visible="true" user="Zverik" uid="221" lat="55.7965727" lon="49.1250105">
+<tag k="historic" v="memorial"/>
+<tag k="name" v="Памятник В.И. Ленину"/>
+</node>
+<node id="4301705355" changeset="69643" timestamp="2016-01-08T14:40:05Z" version="1" visible="true" user="Zverik" uid="221" lat="55.7912044" lon="49.1222602">
+<tag k="historic" v="memorial"/>
+<tag k="name" v="Памятник В.И. Ульянову-Ленину"/>
+</node>
+<node id="4301705402" changeset="69643" timestamp="2016-01-08T14:40:08Z" version="1" visible="true" user="Zverik" uid="221" lat="55.7910116" lon="49.1303252">
+<tag k="historic" v="memorial"/>
+<tag k="name" v="Памятник-бюст Ленину"/>
+</node>
+<node id="4307727993" changeset="109495" timestamp="2018-01-08T17:50:13Z" version="1" visible="true" user="acsd" uid="6277" lat="55.7879536" lon="49.1233323"/>
+<node id="4307727994" changeset="109495" timestamp="2018-01-08T17:50:13Z" version="1" visible="true" user="acsd" uid="6277" lat="55.7883596" lon="49.1237711"/>
+<node id="4307727995" changeset="109495" timestamp="2018-01-08T17:50:13Z" version="1" visible="true" user="acsd" uid="6277" lat="55.7898878" lon="49.1245002"/>
+<node id="4307727996" changeset="109495" timestamp="2018-01-08T17:50:13Z" version="1" visible="true" user="acsd" uid="6277" lat="55.7924735" lon="49.1248956"/>
+<node id="4307727997" changeset="109495" timestamp="2018-01-08T17:50:13Z" version="1" visible="true" user="acsd" uid="6277" lat="55.7944425" lon="49.1251198"/>
+<node id="4307727998" changeset="109495" timestamp="2018-01-08T17:50:13Z" version="1" visible="true" user="acsd" uid="6277" lat="55.7940067" lon="49.1293788"/>
+<node id="4307727999" changeset="109495" timestamp="2018-01-08T17:50:13Z" version="1" visible="true" user="acsd" uid="6277" lat="55.7949098" lon="49.1216080"/>
+<node id="4307733510" changeset="109530" timestamp="2018-01-10T20:21:49Z" version="1" visible="true" user="acsd" uid="6277" lat="55.7877700" lon="49.1277900">
+<tag k="test" v="testing"/>
+</node>
+<node id="4307742318" changeset="109557" timestamp="2018-01-11T15:52:11Z" version="1" visible="true" user="acsd" uid="6277" lat="55.7877700" lon="49.1277900">
+<tag k="test" v="testing"/>
+</node>
+<node id="4307742382" changeset="109560" timestamp="2018-01-11T18:31:51Z" version="1" visible="true" user="acsd" uid="6277" lat="55.7897700" lon="49.1297900"/>
+<node id="4307742383" changeset="109560" timestamp="2018-01-11T18:31:51Z" version="1" visible="true" user="acsd" uid="6277" lat="55.7807700" lon="49.1207900"/>
+<way id="4304212395" changeset="109496" timestamp="2018-01-08T17:51:28Z" version="2" visible="true" user="acsd" uid="6277">
+<nd ref="4307727993"/>
+<nd ref="4307727994"/>
+<nd ref="4307727995"/>
+<nd ref="4307727996"/>
+<nd ref="4307727997"/>
+<tag k="highway" v="secondary"/>
+<tag k="parking:condition:both" v="ticket"/>
+<tag k="parking:lane:both" v="parallel"/>
+</way>
+<way id="4304212396" changeset="109496" timestamp="2018-01-08T17:51:28Z" version="2" visible="true" user="acsd" uid="6277">
+<nd ref="4307727998"/>
+<nd ref="4307727997"/>
+<nd ref="4307727999"/>
+<tag k="highway" v="tertiary"/>
+<tag k="parking:lane:right" v="no_parking"/>
+</way>
+
+</osm>
+`
+
 var legend = [
     { condition: 'disc',        color: 'gold',          text: 'Disc'          },
     { condition: 'no_parking',  color: 'gold',          text: 'No parking'    },
@@ -105,8 +157,13 @@ var legend = [
     { condition: 'residents',   color: 'hotpink',       text: 'For residents' }
 ];
 
+var ways = {};
 var lanes = {};
 var offset = 6;
+
+var version = '0.1'
+
+var change = { osmChange: { $version: '0.6', $generator: 'Parking lane ' + version, modify: { way: [] } } };
 
 var datetime = new Date();
 document.getElementById('datetime-input').value =
@@ -169,6 +226,8 @@ function parseContent(content) {
         if (lanes[obj.$id.toString()] || lanes['-'+obj.$id])
             continue;
 
+        ways[obj.$id] = obj;
+
         var polyline = obj.nd.map(x => nodes[x.$ref]);
 
         for (var side of ['right', 'left']) {
@@ -210,7 +269,8 @@ function getContent(url, callback)
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
     xhr.onload = () => callback(JXON.stringToJs(xhr.responseText));
-    xhr.send();
+    //xhr.send();
+    callback(JXON.stringToJs(osmstatic));
 }
 
 function getConditions(side, tags) {
@@ -290,7 +350,7 @@ function getColorByDate(conditions) {
 function getQueryParkingLanes() {
     var bounds = map.getBounds();
     var bbox = [bounds.getSouth(), bounds.getWest(), bounds.getNorth(), bounds.getEast()].join(',');
-    return '[out:xml];(way[~"^parking:lane:.*"~"."](' + bbox + ');>;);out body;';
+    return '[out:xml];(way[~"^parking:lane:.*"~"."](' + bbox + ');>;);out meta;';
 }
 
 function getQueryHighways() {
@@ -304,26 +364,96 @@ function getQueryOsmId(id) {
     return '[out:xml];(way(id:' + id + ');>;way(id:' + id + ');<;);out meta;';
 }
 
-
 function getPopupContent(osm) {
+    var head = document.createElement('div');
+    head.setAttribute('style', 'min-width:250px');
+
+    var linkOsm = document.createElement('a');
+    linkOsm.setAttribute('target', '_blank');
+    linkOsm.setAttribute('href', 'https://openstreetmap.org/way/' + osm.$id);
+    linkOsm.innerText = 'View in OSM';
+    head.appendChild(linkOsm);
+
+    var editorBlock = document.createElement('span');
+    editorBlock.setAttribute('style', 'float:right');
+    editorBlock.innerText = 'Edit: ';
+
+    var linkJosm = document.createElement('a');
+    linkJosm.setAttribute('target', '_blank');
+    linkJosm.setAttribute('href', urlJosm + urlOverpass + getQueryOsmId(osm.$id));
+    linkJosm.innerText = 'Josm';
+    editorBlock.appendChild(linkJosm);
+    editorBlock.innerHTML += ', ';
+
+    var linkID = document.createElement('a');
+    linkID.setAttribute('target', '_blank');
+    linkID.setAttribute('href', urlID + '&way=' + osm.$id);
+    linkID.innerText = 'iD';
+    editorBlock.appendChild(linkID);
+
+    head.appendChild(editorBlock);
+
+    var form = document.createElement("form");
+    form.setAttribute('id', osm.$id);
+    form.onsubmit = save;
+    
     var regex = new RegExp('^parking:');
-    var result = '';
-
-    result += '<div style="min-width:200px">';
-    result += '<a target="_blank" href="https://openstreetmap.org/way/' + osm.$id + '">View in OSM</a>';
-    result += '<span style="float:right">Edit: ';
-    result += '<a target="_blank" href="' + urlJosm + urlOverpass + getQueryOsmId(osm.$id) + '">Josm</a>';
-    result += ', <a target="_blank" href="' + urlID + '&way=' + osm.$id + '">iD</a>';
-    result += '</span>';
-    result += '</div>';
-
-    result += '<hr>';
-
+    var tagsText = [];
+    var dl = document.createElement('dl');
     for (var tag of osm.tag)
-        if (regex.test(tag.$k))
-            result += tag.$k + ' = ' + tag.$v + '<br />';
-        
-    return result;
+        if (regex.test(tag.$k)) {
+
+            var label = document.createElement('label');
+            label.innerText = tag.$k;
+            var dt = document.createElement('dt');
+            dt.appendChild(label);
+
+            var tagval = document.createElement('input');
+            tagval.setAttribute('type', 'text');
+            tagval.setAttribute('name', tag.$k);
+            tagval.setAttribute('value', tag.$v);
+            var dd = document.createElement('dd');
+            dd.appendChild(tagval);
+
+            dl.appendChild(dt);
+            dl.appendChild(dd);
+        }
+    form.appendChild(dl);
+
+    var submit = document.createElement('input');
+    submit.setAttribute('type', 'submit');
+    submit.setAttribute('value', 'Save');
+    form.appendChild(submit);
+
+    var cancel = document.createElement('input');
+    cancel.setAttribute('type', 'reset');
+    cancel.setAttribute('value', 'Cancel');
+    form.appendChild(cancel);
+
+    var div = document.createElement('div');
+    div.appendChild(head);
+    div.appendChild(document.createElement('hr'));
+    div.appendChild(form);
+
+    return div;
+}
+
+function save(form) {
+    var regex = new RegExp('^parking:');
+    var osm = ways[form.target.id];
+    osm.tag = osm.tag.filter(tag => !regex.test(tag.$k));
+
+    for (var input of form.target)
+        if (input.value != '') {
+            osm.tag.push({ $k: input.name, $v: input.value })
+        }
+
+    delete osm.$user;
+    delete osm.$uid;
+    delete osm.$timestamp;
+
+    change.osmChange.modify.way.push(osm);
+    document.getElementById(osm.$id).innerText = JXON.jsToString(change);
 }
 
 

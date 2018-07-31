0b7bdeb0b61bd33f35903f0e13455bc08b0d7e8b
zetx16 <zetx16@gmail.com>
2018-07-31 20:29:48 +0300

    Overpass query fixed

--- planes.js
+++ planes.js
@@ -527,7 +527,7 @@ function getQueryParkingLanes() {
         var bbox = [bounds.getSouth(), bounds.getWest(), bounds.getNorth(), bounds.getEast()].join(',');
         return editorMode
             ? '[out:xml];(way[highway~"^motorway|trunk|primary|secondary|tertiary|unclassified|residential|service|living_street"](' + bbox + ');)->.a;(.a;.a >;.a <;);out meta;'
-            : '[out:xml];(way[highway][~"^parking:lane:.*"~"."](' + bbox + ');)->.a;(.a;.a >;.a <;);out meta;';
+            : '[out:xml];(way[highway][~"^parking:.*"~"."](' + bbox + ');)->.a;(.a;.a >;.a <;);out meta;';
     }
 }
 

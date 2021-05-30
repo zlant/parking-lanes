0775d02602c9ef0c3a9f94f7d21db9ca522e9228
Almaz <zetx16@gmail.com>
2021-05-30 20:34:15 +0300

    Set input type for datetime of parking state

--- src/parking/controls/datetime.js
+++ src/parking/controls/datetime.js
@@ -6,13 +6,14 @@ export default L.Control.extend({
     onAdd: map => hyper`
         <input id="datetime-input"
                class="leaflet-control-layers control-padding control-bigfont"
-               style="width: 115px"
+               style="width: 150px"
+               type="datetime-local"
                onmousedown=${L.DomEvent.stopPropagation}
                ondblclick=${L.DomEvent.stopPropagation}
                onpointerdown=${L.DomEvent.stopPropagation}>`,
 
     setDatetime(datetime) {
-        document.getElementById('datetime-input').value = dayjs(datetime).format('YYYY-MM-DD HH:mm')
+        document.getElementById('datetime-input').value = dayjs(datetime).format('YYYY-MM-DDTHH:mm')
         return this
     },
 

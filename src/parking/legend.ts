5374b0dd624c631eebcc372fea6c9531e01d4d65
Tobias Jordans <t@tobiasjordans.de>
2021-09-20 21:44:52 +0200

    Add 'no' and 'separate' to legend an interface
    Extend the legend to include an optional supline to explain some more.

--- src/parking/legend.ts
+++ src/parking/legend.ts
@@ -5,10 +5,12 @@ export const legend: ConditionColorDefinition[] = [
     { condition: 'disc',         color: 'yellowgreen',  text: 'Disc' },
     { condition: 'no_parking',   color: 'orange',       text: 'No parking' },
     { condition: 'no_stopping',  color: 'salmon',       text: 'No stopping' },
+    { condition: 'no',           color: 'salmon',       text: 'No parking/stopping', subline: 'Mid section of dual lanes or just not applicable.' },
     { condition: 'free',         color: 'limegreen',    text: 'Free parking' },
     { condition: 'ticket',       color: 'dodgerblue',   text: 'Paid parking' },
     { condition: 'customers',    color: 'greenyellow',  text: 'For customers' },
     { condition: 'residents',    color: 'hotpink',      text: 'For residents' },
     { condition: 'disabled',     color: 'turquoise',    text: 'Disabled' },
+    { condition: 'separate',     color: 'gray',         text: 'Parking street site mapped separately' },
     /* eslint-enable no-multi-spaces */
 ]

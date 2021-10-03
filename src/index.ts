4907f346c7de01a6099bb014157524eb1059fe46
Almaz <zetx16@gmail.com>
2021-10-03 11:10:37 +0300

    Data souce control styled

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import 'normalize.css'
-import './styles/main.scss'
 import { initMap } from './parking/interface'
 import { OurWindow } from './utils/types/interfaces'
+import './styles/main.scss'
 
 (window as OurWindow).map = initMap()
--- src/parking/controls/fetch.ts
+++ src/parking/controls/fetch.ts
@@ -3,31 +3,95 @@ import { hyper } from 'hyperhtml/esm'
 import { OsmDataSource } from '../../utils/types/osm-data'
 
 export default L.Control.extend({
-    onAdd: (map: L.Map, initDataSource: OsmDataSource) => hyper`
-        <div class="leaflet-control-layers control-padding control-bigfont control-button">
-            <div id="download-btn">
-                Fetch parking data
+    onAdd: (map: L.Map) => hyper`
+        <div id="fetch-control"
+             class="fetch-control"
+             onmousedown=${L.DomEvent.stopPropagation}
+             ondblclick=${L.DomEvent.stopPropagation}
+             onpointerdown=${L.DomEvent.stopPropagation}
+             onclick=${L.DomEvent.stopPropagation}>
+            <div class="leaflet-control-layers control-padding control-bigfont control-button">
+                <div class="fetch-control_wrapper">
+                    <div id="download-btn" class="fetch-control_button">
+                        Fetch parking data
+                    </div>
+                    <div class="fetch-control_toggle"
+                         onclick=${handleToggleClick} />
+                </div>
             </div>
-            <select id="data-source-select">
-                <option value="${OsmDataSource.OverpassTurbo}" selected="${initDataSource === OsmDataSource.OverpassTurbo}">From overpass-turbo</option>
-                <option value="${OsmDataSource.OsmOrg}" selected="${initDataSource === OsmDataSource.OsmOrg}">From osm.org</option>
-            </select>
+            <div id="data-source-select" class="fetch-control_items">
+                <div data-value="${OsmDataSource.OverpassTurbo}" 
+                     class="fetch-control_item"
+                     onclick="${handleDataSourceChange}">
+                    From overpass-turbo
+                </div>
+                <div data-value="${OsmDataSource.OsmOrg}"
+                     class="fetch-control_item"
+                     onclick="${handleDataSourceChange}">
+                    From osm.org
+                </div>
+            </div>
         </div>`,
 
-    setFetchDataBtnClickListener(listener: any) {
+    setFetchDataBtnClickListener(listener: (e?: MouseEvent) => any) {
         document.getElementById('download-btn')!.onclick = listener
         return this
     },
 
+    setFetchDataBtnText(newText: string) {
+        (document.getElementById('download-btn') as HTMLButtonElement)
+            .innerText = newText
+    },
+
     setDataSource(dataSource: OsmDataSource) {
-        (document.getElementById('data-source-select') as HTMLSelectElement).value = dataSource.toString()
+        updateItemsStyles(dataSource)
         return this
     },
 
-    setDataSourceChangeListener(listener: any) {
-        document.getElementById('data-source-select')!.onchange =
-            (e: Event | any) => listener(parseInt(e.target.value))
-
+    setDataSourceChangeListener(listener: (dataSource: OsmDataSource) => void) {
+        dataSourceChangeListener = listener
         return this
     },
 })
+
+// data source select list opened state
+
+let opened: boolean = true
+
+function setOpenedState(newState: boolean) {
+    if (newState)
+        document.getElementById('fetch-control')?.classList.add('opened')
+    else
+        document.getElementById('fetch-control')?.classList.remove('opened')
+
+    opened = newState
+}
+
+function handleToggleClick(e: MouseEvent) {
+    setOpenedState(!opened)
+}
+
+// data source change handlers
+
+let dataSourceChangeListener: (dataSource: OsmDataSource) => void = () => {}
+
+function handleDataSourceChange(e: Event) {
+    const value = (e.target as HTMLElement).dataset.value
+    if (!value)
+        return
+
+    const dataSource = parseInt(value)
+    dataSourceChangeListener(dataSource)
+    updateItemsStyles(dataSource)
+    setOpenedState(false)
+}
+
+function updateItemsStyles(dataSource: OsmDataSource) {
+    document.querySelectorAll('#data-source-select > .fetch-control_item').forEach(el => {
+        const htmlEl = el as HTMLElement
+        if (htmlEl.dataset.value === dataSource.toString())
+            htmlEl.classList.add('fetch-control_item--selected')
+        else
+            htmlEl.classList.remove('fetch-control_item--selected')
+    })
+}

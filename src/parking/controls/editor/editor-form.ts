573650ead1e8346f34c221d9627e65d7f617e632
Almaz <zetx16@gmail.com>
2022-03-13 16:15:25 +0300

    Show hidden inputs if exists value

--- src/parking/controls/editor/editor-form.ts
+++ src/parking/controls/editor/editor-form.ts
@@ -180,7 +180,7 @@ function getTagInput(osm: OsmWay, side: string, parkingType: string, tagTemplate
 
     return hyper`
         <tr id="${tag}"
-            style=${{ display: hide ? 'none' : null }}>
+            style=${{ display: hide && !value ? 'none' : null }}>
             <td><label title="${tag}">${label}</label></td>
             <td>
                 ${input}
@@ -266,7 +266,7 @@ function handleLaneTagInput(e: Event) {
     const surfaceTr = document.getElementById(`parking:lane:${side}:surface`)
     if (['parallel', 'diagonal', 'perpendicular', 'marked', 'yes'].includes(el.value))
         showElement(surfaceTr!.id)
-    else
+    else if (!surfaceTr?.querySelector('input')?.value)
         hideElement(surfaceTr!.id)
 }
 
@@ -275,7 +275,7 @@ function handleConditionTagInput(e: Event) {
     const side = el.name.split(':')[2]
     const maxstayTr = document.getElementById(`parking:condition:${side}:maxstay`)
 
-    if (el.value === 'disc')
+    if (el.value === 'disc' && !maxstayTr?.querySelector('input')?.value)
         hideElement(maxstayTr!.id)
     else
         showElement(maxstayTr!.id)
@@ -286,7 +286,7 @@ function handleTimeIntervalTagInput(e: Event) {
     const side = el.name.split(':')[2]
     const defaultConditionTr = document.getElementById(`parking:condition:${side}:default`)
 
-    if (el.value === '')
+    if (el.value === '' && !defaultConditionTr?.querySelector('input')?.value)
         hideElement(defaultConditionTr!.id)
     else
         showElement(defaultConditionTr!.id)

d7de1b06d08e276ad50b2d6a7caa93ad8110b56c
Almaz <zetx16@gmail.com>
2022-03-16 22:10:46 +0300

    Fix conditional tag parsing; Add unit tests for conditional tag parser

--- src/test/parking-area.test.ts
+++ src/test/parking-area.test.ts
@@ -2,14 +2,14 @@ import { getConditions } from '../parking/parking-area'
 import { OsmTags } from '../utils/types/osm-data'
 
 describe('#getConditions()', () => {
-    test('should return no intervals and default as free when no tags', async() => {
+    test('should return no conditional values and default as free when no tags', async() => {
         const tags: OsmTags = {}
         const conditions = getConditions(tags)
-        expect(conditions).toStrictEqual({ default: 'free', intervals: [] })
+        expect(conditions).toStrictEqual({ default: 'free', conditionalValues: [] })
     })
     test('should return ticket when fee=yes', async() => {
         const tags: OsmTags = { fee: 'yes' }
         const conditions = getConditions(tags)
-        expect(conditions).toStrictEqual({ default: 'ticket', intervals: [] })
+        expect(conditions).toStrictEqual({ default: 'ticket', conditionalValues: [] })
     })
 })

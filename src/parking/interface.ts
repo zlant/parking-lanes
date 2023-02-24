02e0de6d19d763e8c76783747811ba45787528d4
Almaz <zetx16@gmail.com>
2023-02-24 22:57:52 +0300

    Up version 0.8.1

--- src/parking/interface.ts
+++ src/parking/interface.ts
@@ -40,7 +40,7 @@ import { parseParkingArea, updateAreaColorsByDate } from './parking-area'
 import { parseParkingPoint, updatePointStylesByZoom } from './parking-point'
 
 const editorName = 'PLanes'
-const version = '0.8.0'
+const version = '0.8.1'
 
 let editorMode = false
 const useDevServer = false

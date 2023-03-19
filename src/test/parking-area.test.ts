f825fe292db47df11a1ca004733334082a93738a
Almaz <zetx16@gmail.com>
2023-03-19 13:46:48 +0300

    Add tests

--- src/test/parking-area.test.ts
+++ src/test/parking-area.test.ts
@@ -12,6 +12,7 @@ describe('#getConditions()', () => {
         }
         expect(expectedConditions).toStrictEqual(receivedConditions)
     })
+
     test('should return ticket when fee=yes', async() => {
         const tags: OsmTags = { fee: 'yes' }
         const receivedConditions = getConditions(tags)
@@ -21,6 +22,7 @@ describe('#getConditions()', () => {
         }
         expect(expectedConditions).toStrictEqual(receivedConditions)
     })
+
     test('fee={time_interval}', async() => {
         const tags: OsmTags = { fee: 'Mo-Fr 08:00-19:00' }
         const receivedConditions = getConditions(tags)
@@ -33,6 +35,7 @@ describe('#getConditions()', () => {
         }
         expect(expectedConditions).toStrictEqual(receivedConditions)
     })
+
     test('fee:conditional=yes @ ({time_interval})', async() => {
         const tags: OsmTags = { 'fee:conditional': 'yes @ (Mo-Fr 08:00-19:00)' }
         const receivedConditions = getConditions(tags)
@@ -45,6 +48,7 @@ describe('#getConditions()', () => {
         }
         expect(expectedConditions).toStrictEqual(receivedConditions)
     })
+
     test('fee:conditional=no @ ({time_interval})', async() => {
         const tags: OsmTags = { 'fee:conditional': 'no @ (Mo-Fr 08:00-19:00)' }
         const receivedConditions = getConditions(tags)
@@ -57,6 +61,7 @@ describe('#getConditions()', () => {
         }
         expect(expectedConditions).toStrictEqual(receivedConditions)
     })
+
     test('fee:conditional=no @ ({time_interval})', async() => {
         const tags: OsmTags = { 'fee:conditional': 'yes @ (stay > 2h)' }
         const receivedConditions = getConditions(tags)
@@ -64,6 +69,71 @@ describe('#getConditions()', () => {
             default: 'disc',
             conditionalValues: [{
                 parkingCondition: 'ticket',
+                condition: null,
+            }],
+        }
+        expect(expectedConditions).toStrictEqual(receivedConditions)
+    })
+
+    test('maxstay and fee=no', async() => {
+        const tags: OsmTags = {
+            'parking:right': 'street_side',
+            'parking:right:fee': 'no',
+            'parking:right:maxstay': '15 minutes',
+        }
+        const receivedConditions = getConditions(tags, 'right')
+        const expectedConditions: ParkingConditions = {
+            default: 'disc',
+            conditionalValues: [],
+        }
+        expect(expectedConditions).toStrictEqual(receivedConditions)
+    })
+
+    test('access=private', async() => {
+        const tags: OsmTags = {
+            'parking:left': 'street_side',
+            'parking:left:access': 'private',
+        }
+        const receivedConditions = getConditions(tags, 'left')
+        const expectedConditions: ParkingConditions = {
+            default: 'no_stopping',
+            conditionalValues: [],
+        }
+        expect(expectedConditions).toStrictEqual(receivedConditions)
+    })
+
+    test('maxstay:conditional and fee:conditional', async() => {
+        const tags: OsmTags = {
+            'parking:right': 'yes',
+            'parking:right:fee': 'no',
+            'parking:right:fee:conditional': 'yes @ (Mo-Fr 08:00-18:00; Sa 08:00-15:00)',
+            'parking:right:maxstay:conditional': '30 minutes @ (Mo-Fr 08:00-18:00; Sa 08:00-15:00)',
+        }
+        const receivedConditions = getConditions(tags, 'right')
+        const expectedConditions: ParkingConditions = {
+            default: 'free',
+            conditionalValues: [{
+                parkingCondition: 'ticket',
+                condition: null,
+            }, {
+                parkingCondition: 'disc',
+                condition: null,
+            }],
+        }
+        expect(expectedConditions).toStrictEqual(receivedConditions)
+    })
+
+    test('loading_only', async() => {
+        const tags: OsmTags = {
+            'parking:right:restriction': 'no_parking',
+            'parking:right:restriction:conditional': 'loading_only @ (Mo-Fr 08:00-11:00)',
+            'parking:right:restriction:reason': 'loading_zone',
+        }
+        const receivedConditions = getConditions(tags, 'right')
+        const expectedConditions: ParkingConditions = {
+            default: 'no_parking',
+            conditionalValues: [{
+                parkingCondition: 'loading_only',
                 condition: null,
             }],
         }
